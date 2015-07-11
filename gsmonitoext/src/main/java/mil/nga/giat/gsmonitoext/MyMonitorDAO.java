fc0b23e380445df1fb13eaed966184fec42da041
João Martins <joaomartins27396@gmail.com>
2015-07-11 13:52:13 +0100

    modifications

---
+++ gsmonitoext/src/main/java/mil/nga/giat/gsmonitoext/MyMonitorDAO.java
@@ -0,0 +1,352 @@
+package mil.nga.giat.gsmonitoext;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.geoserver.monitor.MonitorConfig;
+import org.geoserver.monitor.MonitorDAO;
+import org.geoserver.monitor.RequestData;
+import org.geoserver.monitor.RequestDataVisitor;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFactorySpi;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.FeatureReader;
+import org.geotools.data.FeatureWriter;
+import org.geotools.data.Query;
+import org.geotools.data.Transaction;
+import org.geotools.feature.SchemaException;
+import org.geotools.filter.FilterFactoryImpl;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.opengis.filter.expression.Expression;
+import org.opengis.filter.spatial.BBOX;
+import org.opengis.geometry.BoundingBox;
+import org.opengis.geometry.Geometry;
+import org.opengis.geometry.coordinate.Polygon;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+public class MyMonitorDAO implements MonitorDAO {
+
+	public static final String TYPENAME = "requestDataFeature";
+	private DataStore dataStore = null;
+
+	private SimpleFeatureType featureType = null;
+
+	String dataStoreTypeName = TYPENAME;
+
+	// need a way to set/get this via Spring setter/getter
+	private Map<String, Serializable> dataStoreParams;
+
+	// myData.createSchema( featureType );
+
+	@Override
+	public String getName() {
+		return "SimpleFeature Monitor";
+	}
+
+	public String getDataStoreTypeName() {
+		return dataStoreTypeName;
+	}
+
+	public void setDataStoreTypeName(String dataStoreTypeName) {
+		this.dataStoreTypeName = dataStoreTypeName;
+	}
+
+	public Map<String, Serializable> getDataStoreParams() {
+		return dataStoreParams;
+	}
+
+	public void setDataStoreParams(Map<String, Serializable> dataStoreParams) {
+		this.dataStoreParams = dataStoreParams;
+	}
+
+	@Override
+	public void init(MonitorConfig config) {
+
+		// example...needs more
+		try {
+			featureType = DataUtilities
+					.createType(
+							dataStoreTypeName,
+							"envelope:Polygon,id:java.lang.Long,queryString:String,path:String,startTime:Date, endTime:Date, totalTime:java.lang.Long");
+		} catch (SchemaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// HOW TO FIND OR CREATE A DATA STORE
+
+		// Experiment here to see what works best using the page I gave you.
+		// For testing, somehow parameterize this to use a shape file store.
+		// The latest version of documentation suggests using a Catalog.
+
+		final Map<String, Serializable> params = dataStoreParams;
+
+		for (Iterator<?> i = DataStoreFinder.getAvailableDataStores(); i
+				.hasNext();) {
+			DataStoreFactorySpi factory = (DataStoreFactorySpi) i.next();
+
+			try {
+				if (factory.canProcess(params)) {
+
+					this.dataStore = factory.createDataStore(params);
+
+					if (!dataStore.getNames().contains(
+							this.featureType.getName())) {
+						dataStore.createSchema(this.featureType);
+					}
+				}
+
+			} catch (Throwable warning) {
+				System.out.println(factory.getDisplayName() + " failed:"
+						+ warning);
+				warning.printStackTrace();
+			}
+		}
+
+	}
+
+	// When is this used...do we need to implement something here...like setting
+	// certain attributes
+	@Override
+	public RequestData init(RequestData data) {
+
+		try {
+			featureType = DataUtilities
+					.createType(
+							dataStoreTypeName,
+							"envelope:Polygon,id:java.lang.Long,queryString:String,path:String,startTime:Date, endTime:Date, totalTime:java.lang.Long");
+		} catch (SchemaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		final Map<String, Serializable> params = dataStoreParams;
+
+		for (Iterator<?> i = DataStoreFinder.getAvailableDataStores(); i
+				.hasNext();) {
+			DataStoreFactorySpi factory = (DataStoreFactorySpi) i.next();
+
+			try {
+				if (factory.canProcess(params)) {
+
+					this.dataStore = factory.createDataStore(params);
+
+					if (!dataStore.getNames().contains(
+							this.featureType.getName())) {
+						dataStore.createSchema(this.featureType);
+					}
+				}
+
+			} catch (Throwable warning) {
+				System.out.println(factory.getDisplayName() + " failed:"
+						+ warning);
+				warning.printStackTrace();
+			}
+		}
+
+		save(data);
+
+		return data;
+	}
+
+	@Override
+	public void add(RequestData data) {
+		this.save(data);
+	}
+
+	@Override
+	public void update(RequestData data) {
+		Transaction t = new DefaultTransaction("handle");
+
+		FilterFactoryImpl factory = new FilterFactoryImpl();
+		Expression exp1 = factory.property("id");
+		Expression exp2 = factory.literal(data.getId());
+		Filter equals = factory.equal(exp1, exp2, false);
+
+		try (FeatureWriter<SimpleFeatureType, SimpleFeature> fw = dataStore
+				.getFeatureWriter(dataStoreTypeName, equals, t)) {
+			while (fw.hasNext()) {
+				SimpleFeature newFeature = fw.next();
+				toSimpleFeature(newFeature, data);
+				fw.write();
+			}
+
+			t.commit();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				t.rollback();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void toSimpleFeature(SimpleFeature featureToUpdate,
+			RequestData data) {
+
+		// "envelope:Polygon,id:java.lang.Long,queryString:String,path:String,startTime:Date, endTime:Date, totalTime:java.lang.Long");
+
+		if (data.getBbox() != null)
+			featureToUpdate.setAttribute("Polygon",
+					JTS.toGeometry(data.getBbox()));
+
+		featureToUpdate.setAttribute("id", data.getId());
+		featureToUpdate.setAttribute("path", data.getPath());
+
+		if (data.getStartTime() != null)
+			featureToUpdate.setAttribute("startTime", data.getStartTime());
+		if (data.getEndTime() != null)
+			featureToUpdate.setAttribute("endTime", data.getEndTime());
+		featureToUpdate.setAttribute("totalTime", data.getTotalTime());
+
+		//
+	}
+
+	protected void toRequestData(SimpleFeature feature, RequestData dataToUpdate) {
+
+		// the converstion to bbox I don't know exactly how to do that
+
+		Polygon pol = (Polygon) feature.getAttribute("Polygon");
+
+		GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+		Geometry geo = (Geometry) geometryFactory.buildGeometry(pol
+				.getSpanningSurface());
+
+		dataToUpdate.setBbox((BoundingBox) geo.getEnvelope());
+		dataToUpdate.setId((long) feature.getAttribute("id"));
+		dataToUpdate.setPath((String) feature.getAttribute("path"));
+		dataToUpdate.setStartTime((Date) feature.getAttribute("startTime"));
+		dataToUpdate.setEndTime((Date) feature.getAttribute("endTime"));
+		dataToUpdate.setTotalTime((long) feature.getAttribute("totalTime"));
+
+		//
+	}
+
+	@Override
+	public void save(RequestData data) {
+		Transaction t = new DefaultTransaction("handle");
+		try (FeatureWriter<SimpleFeatureType, SimpleFeature> fw = dataStore
+				.getFeatureWriterAppend(dataStoreTypeName, t)) {
+			SimpleFeature newFeature = fw.next();
+			toSimpleFeature(newFeature, data);
+			fw.write();
+			t.commit();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				t.rollback();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public RequestData getRequest(long id) {
+		Transaction t = new DefaultTransaction("handle");
+
+		FilterFactoryImpl factory = new FilterFactoryImpl();
+		Expression exp1 = factory.property("id");
+		Expression exp2 = factory.literal(id);
+		Filter equals = factory.equal(exp1, exp2, false);
+		RequestData data = new RequestData();
+		try (FeatureReader<SimpleFeatureType, SimpleFeature> fw = dataStore
+				.getFeatureReader(new Query(dataStoreTypeName, equals), t)) {
+
+			if (fw.hasNext()) {
+				toRequestData(fw.next(), data);
+
+			}
+			t.commit();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				t.rollback();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return data;
+	}
+
+	@Override
+	public List<RequestData> getOwsRequests() {
+		// TODO Auto-generated method stub...ignore for now!!!
+		return new ArrayList<RequestData>();
+	}
+
+	@Override
+	public List<RequestData> getOwsRequests(String service, String operation,
+			String version) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public List<RequestData> getRequests(org.geoserver.monitor.Query query) {
+		// TODO Auto-generated method stub....handle null query
+		return null;
+	}
+
+	@Override
+	public void getRequests(org.geoserver.monitor.Query query,
+			RequestDataVisitor visitor) {
+
+		final Iterator<RequestData> it = getIterator(query);
+		while (it.hasNext()) {
+			visitor.visit(it.next());
+		}
+	}
+
+	@Override
+	public long getCount(org.geoserver.monitor.Query query) {
+		// TODO
+		return 0;
+	}
+
+	@Override
+	public Iterator<RequestData> getIterator(org.geoserver.monitor.Query query) {
+		return getRequests(query).iterator();
+	}
+
+	@Override
+	public List<RequestData> getRequests() {
+		return getRequests(null);
+	}
+
+}
---
+++ gsmonitoext/src/test/java/mil/nga/gsmonitoext/MyMonitorDAOTest.java
@@ -0,0 +1,77 @@
+package mil.nga.gsmonitoext;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+
+import mil.nga.giat.gsmonitoext.MyMonitorDAO;
+
+import org.apache.commons.io.FileUtils;
+import org.geoserver.monitor.MonitorConfig;
+import org.geoserver.monitor.RequestData;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.PrecisionModel;
+
+public class MyMonitorDAOTest
+{
+
+	MyMonitorDAO dao = new MyMonitorDAO();
+
+	GeometryFactory factory = new GeometryFactory(
+			new PrecisionModel(
+					PrecisionModel.FIXED));
+	@Before
+	public void setup()
+			throws IOException {
+
+		File dataDir = new File(
+				"testdata");
+		if (dataDir.exists()) FileUtils.deleteDirectory(dataDir);
+	//	if (!dataDir.exists()) dataDir.mkdirs();
+		Map params = new HashMap();
+		params.put("dbtype", "h2");
+	    params.put("database", "testdata/db");
+		MonitorConfig config = new MonitorConfig();
+		dao.setDataStoreTypeName("MonitorRequestData");
+		dao.setDataStoreParams(params);
+		dao.init(config);
+	}
+
+	/*
+	@Test void testMappings() {
+		// need to add a protected feature type access method to the DAO
+		final SimpleFeatureBuilder builder = new SimpleFeatureBuilder(dao.getFeatureType());
+		final SimpleFeature feature = builder.buildFeature("123");
+		final RequestData request = new RequestData();
+		
+		// put some test data in each attribute..ignore Error and body[] for now
+		request.setBbox(factory.createPolygon...etc...));
+		request.setHost("localhost");
+		...etc....
+		
+		dao.toSimpleFeature(feature, request);
+		final RequestData copyOfRequest = new RequestData();
+		dao.toRequestData(feature, copyOfRequest);
+		
+		// did the bi-directional mapping work...check each attribute
+		assertEquals(request.getHost(), copyOfRequest.getHost());
+		...etc....
+	}*/
+	
+	@Test
+	public void test()
+			throws IOException {
+
+		dao.add(new RequestData());
+	}
+}
