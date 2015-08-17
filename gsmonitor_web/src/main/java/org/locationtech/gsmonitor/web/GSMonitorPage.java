80f64129dea80e8dd2eba70c488aebd415b76e39
João Martins <joaomartins27396@gmail.com>
2015-08-17 18:50:43 +0100

    web progrecion
    Is needed to create the FeatureMonitorDAO with the properties of the
    datastore

--- gsmonitor_web/src/main/java/org/locationtech/gsmonitor/web/GSMonitorPage.java
+++ gsmonitor_web/src/main/java/org/locationtech/gsmonitor/web/GSMonitorPage.java
@@ -1,27 +1,37 @@
 package org.locationtech.gsmonitor.web;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
-import org.apache.wicket.extensions.markup.html.form.select.Select;
-import org.apache.wicket.extensions.markup.html.form.select.SelectOption;
+import javax.swing.text.html.Option;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.monitor.MonitorConfig;
+import org.geoserver.monitor.MonitorDAO;
 import org.geoserver.web.GeoServerBasePage;
+import org.geotools.util.logging.Logging;
 import org.locationtech.gsmonitor.*;
 
 public class GSMonitorPage extends GeoServerBasePage {
 
-	private String selected = "jQuery";
-
+	
+	private String selected = "selected";
+	
+	
 	public GSMonitorPage() {
 		FeatureMonitorDAO dao = new FeatureMonitorDAO();
 
-		List<DataStoreInfo> dataStores = getCatalog().getStores(
+		final List<DataStoreInfo> dataStores = getCatalog().getStores(
 				DataStoreInfo.class);
 		List store = new ArrayList<String>();
 
@@ -33,12 +43,78 @@ public class GSMonitorPage extends GeoServerBasePage {
 			}
 		}
 
-		DropDownChoice<String> options = new DropDownChoice<String>("options",
-				store);
-		add(options);
+		
 
 		add(new Label("hellolabel", "Hello World!"));
+		
+		
+		
 
+		Form f = new Form("form", new Model(this));
+        f.setOutputMarkupId(true);
+        add(f);
+        
+        
+        
+        
+        final DropDownChoice<String> option = new DropDownChoice<String>("options", new Model(selected),
+				store){
+			@Override
+			protected void onSelectionChanged(String newSelection) {
+
+            	LOGGER.info("Ok button pressed dropdownChoice "+newSelection);
+				setSelected(newSelection);
+				super.onSelectionChanged(newSelection);
+			}
+		};
+
+		f.add(option);
+		f.add(new AjaxButton("run", f) {
+            protected void onSubmit(AjaxRequestTarget target, Form f) {
+            	String selected = option.getModelObject();
+            	if(selected!=null){
+            		DataStoreInfo data = null;
+            		for(DataStoreInfo dsi: dataStores){
+            			if(dsi.getName().equals(selected)){
+            				DataStoreInfo dataStoreSelected = dsi;
+            				
+            				MonitorConfig config;
+							try {
+								config = new MonitorConfig(getCatalog().getResourceLoader());
+								FeatureMonitorDAO dao = new FeatureMonitorDAO();
+	            				dao.init(config);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+            				
+            				
+            				
+            				return;
+            			}
+            		}
+            	}
+            	
+            	LOGGER.info("Ok button pressed form "+option.getModelObject());
+            }
+        });
+		
+		
+		
+		
+
+		
+		
+		
+
+	}
+	
+	
+	private void setSelected(String string){
+		this.selected = string;
+	}
+	private String getSelected(){
+		return selected;
 	}
 
 }
