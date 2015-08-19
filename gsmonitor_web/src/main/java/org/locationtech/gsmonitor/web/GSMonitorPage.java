06b91a2775be7340963df31b52b4ef40c04ccb9c
João Martins <joaomartins27396@gmail.com>
2015-08-19 03:36:51 +0100

    page changes

--- gsmonitor_web/src/main/java/org/locationtech/gsmonitor/web/GSMonitorPage.java
+++ gsmonitor_web/src/main/java/org/locationtech/gsmonitor/web/GSMonitorPage.java
@@ -3,33 +3,24 @@ package org.locationtech.gsmonitor.web;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.swing.text.html.Option;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.monitor.MonitorConfig;
-import org.geoserver.monitor.MonitorDAO;
-import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.GeoServerSecuredPage;
-import org.geotools.util.logging.Logging;
 import org.locationtech.gsmonitor.*;
 
 public class GSMonitorPage extends GeoServerSecuredPage {
 
 	private String selectedDataStoreName = "selected";
 	private String dataStoreInUse = "";
+	private String message = "";
 
 	public GSMonitorPage() {
 
@@ -37,9 +28,10 @@ public class GSMonitorPage extends GeoServerSecuredPage {
 			final MonitorConfig config = new MonitorConfig(getCatalog()
 					.getResourceLoader());
 
-			final FeatureMonitorDAO dao = FeatureMonitorDAO.lookupMonitor(config);
+			final FeatureMonitorDAO dao = FeatureMonitorDAO
+					.lookupMonitor(config);
 			dao.init(config);
-			final String configuredStoreID =  dao.getStoreID();
+			final String configuredStoreID = dao.getStoreID();
 
 			final List<DataStoreInfo> dataStores = getCatalog().getStores(
 
@@ -62,26 +54,39 @@ public class GSMonitorPage extends GeoServerSecuredPage {
 				}
 
 			}
-
 			
-
+			
+			add(new Label("adminMensage", Model.of("Mensages: "+message)));
+			
 			Form form = new Form("form", new Model(this));
 
 			form.setOutputMarkupId(true);
 
 			add(form);
 			
-			form.add(new Label("dsName", Model.of("DataStore in use: "+dataStoreInUse)));
+			
+			
+
+			form.add(new Label("dsName", Model.of("DataStore in use: "
+					+ dataStoreInUse)));
 
 			final DropDownChoice<String> option = new DropDownChoice<String>(
-					"options", new Model(selectedDataStoreName),
-					store);
+					"options", new Model(selectedDataStoreName), store);
 			form.add(option);
+
+			
+
+			form.add(new Label("featureType", Model.of("FeatureType: "+dao
+					.getDataStoreTypeName())));
+
+			final TextField<String> newFeatureType = new TextField<String>(
+					"newFeatureType", new Model());
+			form.add(newFeatureType);
 
 			form.add(new AjaxButton("save", form) {
 
 				protected void onSubmit(AjaxRequestTarget target, Form f) {
-
+					
 					String selected = option.getModelObject();
 
 					if (selected != null) {
@@ -91,39 +96,23 @@ public class GSMonitorPage extends GeoServerSecuredPage {
 						for (DataStoreInfo dsi : dataStores) {
 
 							if (dsi.getName().equals(selected)) {
+								//set the featureType
+								dao.setDataStoreTypeName(newFeatureType.getInputName());
+								//set the dataStore
 								dao.updateDataStoreProperties(dsi.getName(),
 										dsi.getConnectionParameters());
 								dao.init(config);
 								dataStoreInUse = dsi.getName();
-								return;
 
+								message = "Monitor Configured";
+								return;
 							}
 
 						}
+						
+						message = "dataStore not finded";
 
 					}
-
-				}
-
-			});
-			
-
-			form.add(new Label("featureType", Model.of(dao.getDataStoreTypeName())));
-			
-			
-			
-			
-
-			final TextField<String> newFeatureType = new TextField<String>("newFeatureType",new Model());
-			form.add(newFeatureType);
-			
-			
-			form.add(new AjaxButton("saveFeatureType", form) {
-
-				protected void onSubmit(AjaxRequestTarget target, Form f) {
-
-					LOGGER.log(Level.SEVERE, "deteted"+newFeatureType.getInputName());
-					dao.setDataStoreTypeName(newFeatureType.getInputName());
 
 				}
 
@@ -134,8 +123,5 @@ public class GSMonitorPage extends GeoServerSecuredPage {
 		}
 
 	}
-
-
-
 
 }
